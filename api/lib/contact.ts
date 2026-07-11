ec521e6838a413f6a7471d7f679f74254ec90e4c
sanjanashetti13 <sanjanarshetti1306@gmail.com>
2026-07-11 13:14:15 +0530

    Fix contact API syntax error blocking production build

--- api/lib/contact.ts
+++ api/lib/contact.ts
@@ -59,8 +59,9 @@ export async function handleContactPost(body: unknown) {
 
 function escapeHtml(value: string): string {
   return value
-    .replaceAll("&", "&amp;")
-    .replaceAll("<", "&lt;")
-    .replaceAll(">", "&gt;")
-    .replaceAll('"', "&quot;")
-    .replaceAll("'", "&#39;");
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#39;");
+}
